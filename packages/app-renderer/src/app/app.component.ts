ea625d943378e8cd87716f14aaf1a16939e6e00e
William Chelman <WilliamChelman@users.noreply.github.com>
2017-10-29 14:28:11 +0100

    add example use of electron in renderer process

--- packages/app-renderer/src/app/app.component.ts
+++ packages/app-renderer/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+const remote = window.require("electron").remote;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+
+  click() {
+    remote.dialog.showOpenDialog({ title: "Hello", message: "World!" });
+  }
 }
